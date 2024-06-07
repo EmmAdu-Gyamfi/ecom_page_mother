eb7a85e6f1ae00dfe936acdeedd39377f3498705
Gary Nartey <garynartey@hotmail.com>
2024-06-07 13:40:02 +0100

    updated logic...

---
+++ lib/home/about.dart
@@ -0,0 +1,24 @@
+import '../global/typedef.dart';
+
+class About{
+//--------------------------------------------------------------------------
+// key definition for product object
+
+  Map<dynamic, dynamic> productKeyList = {
+    "title": "CTO",
+    "description": "Technical",
+    "director": [
+      {
+        "dir_name": "",
+        "dir_title": "",
+        "dir_image": "https://komlynk.com/dir_one.png"
+      },
+      {
+        "dir_name": "",
+        "dir_title": "",
+        "dir_image": "https://komlynk.com/dir_one.png"
+      }
+    ],
+    "banner": "https://komlynk.com/dir_one.png",
+  };
+}
