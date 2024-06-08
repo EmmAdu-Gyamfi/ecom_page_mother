d5042f47f01228b50427a1dc08d3d5c3acdc1708
Othniel Otuo Twumasi <72864811+bluevolvo@users.noreply.github.com>
2024-06-08 14:55:13 +0000

    Working on the changes in the loginPage

---
+++ lib/global/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static final TextStyle textH6 = GoogleFonts.plusJakartaSans(
+    color: Color(0xFF57636C),
+    fontSize: 14,
+    letterSpacing: 0,
+    fontWeight: FontWeight.w500,
+  );
+
+// static final TextStyle hintText =
+}
